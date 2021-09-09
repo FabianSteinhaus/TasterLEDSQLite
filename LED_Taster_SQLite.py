d5d0db8ece6267b5d17c8575fc27c9c11498680f
Fabian Steinhaus <fabian.steinhaus88@gmail.com>
2021-09-09 11:38:42 +0200

    Create Table

--- LED_Taster_SQLite.py
+++ LED_Taster_SQLite.py
@@ -1,1 +1,16 @@
 import sqlite3
+import datetime
+
+connection = sqlite3.connect("led_taster.db")
+c = connection.cursor()
+
+c.execute("""CREATE TABLE IF NOT EXISTS led_status
+            (
+                id INTEGER PRIMARY KEY,
+                led_status BOOLEAN,
+                zeitstempel DATETIME
+            )""")
+
+            
+connection.commit()
+connection.close()
