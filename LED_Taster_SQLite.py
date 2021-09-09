4a7d8ca054aee5358a8d58e44a649b1dbf6ae2a8
Fabian Steinhaus <fabian.steinhaus88@gmail.com>
2021-09-09 12:02:38 +0200

    Insert Test Data

--- LED_Taster_SQLite.py
+++ LED_Taster_SQLite.py
@@ -3,6 +3,8 @@ import datetime
 
 connection = sqlite3.connect("led_taster.db")
 c = connection.cursor()
+on_off = False
+zeitpunkt = datetime.now
 
 c.execute("""CREATE TABLE IF NOT EXISTS led_status
             (
@@ -11,6 +13,8 @@ c.execute("""CREATE TABLE IF NOT EXISTS led_status
                 zeitstempel DATETIME
             )""")
 
-            
+c.execute("INSERT INTO led_status (led_status, zeitstempel) values (?,?)",
+        (on_off, zeitpunkt))
+
 connection.commit()
 connection.close()
